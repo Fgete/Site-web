b33ca40603c2ab65e61fc9e3a138592f6ef0ec3d
Lag <f.gete@ludus-academie.com>
2020-10-13 11:21:10 +0200

    index 2.6

--- js/index.js
+++ js/index.js
@@ -4,34 +4,44 @@ var nightMode = false;
 function toggleNightMode(){
 	nightMode = !nightMode;
 
+	// NIGHT MODE
 	slider = document.getElementById("nightModeSlider");
 	capsule = document.getElementById("nightModeCapsule");
 	lune = document.getElementById("nightModeLune");
+
+	// BODY BACKGROUND
 	body = document.getElementById("body");
-	chest = document.getElementsByClassName("chest");
+
+	// FONTS
+	darkFont = document.getElementsByClassName("darkFont");
+	lightFont = document.getElementsByClassName("lightFont");
 
 	margin = slider.style.marginRight;
 	color = capsule.style.backgroundColor;
 	bodyColor = body.style.backgroundColor;
-	bodyFontColor = chest[0].style.color;
+	darkFontColor = "var(--color_MainBlack)";
+	lightFontColor = "var(--color_MainWhite)";
 
 	if (nightMode){
 		margin = "1.5em";
 		color = "var(--color_MainOrange)";
 		bodyColor = "var(--color_MainBlack)";
-		bodyFontColor = "var(--color_MainWhite)";
+		darkFontColor = "var(--color_MainWhite)";
+		lightFontColor = "var(--color_MainOrange)";
 	}else{
 		margin = "0";
 		color = "var(--color_MainWhite)";
 		bodyColor = "var(--color_MainWhite)";
-		bodyFontColor = "var(--color_MainBlack)";
+		darkFontColor = "var(--color_MainBlack)";
+		lightFontColor = "var(--color_MainWhite)";
 	}
 
 	slider.style.marginRight = margin;
 	capsule.style.backgroundColor = color;
 	lune.style.backgroundColor = color;
 	body.style.backgroundColor = bodyColor;
-	for (var i = 0; i < chest.length; i++) {
-		chest[i].style.color = bodyFontColor;
-	}
+	for (var i = 0; i < darkFont.length; i++)
+		darkFont[i].style.color = darkFontColor;
+	for (var i = 0; i < lightFont.length; i++)
+		lightFont[i].style.color = lightFontColor;
 }
