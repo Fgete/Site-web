6a44416662d6fdedd7cd2734e1487bfce233967f
Lag <f.gete@ludus-academie.com>
2021-03-01 12:21:56 +0100

    site 3.8.2 contact done

--- js/index - build.js
+++ js/index - build.js
@@ -2,50 +2,6 @@
 // We use Session Storage in order to show the loading
 // screen only once per user session
 
-// --- LOADING BUILDING ---
-
-// If the loading screen has never been closed, build it
-if (sessionStorage.getItem('loadingClosed') != 1){
-	// Loading background
-	var loadingBack = document.createElement("div");
-	loadingBack.setAttribute("id", "loadingBack");
-	document.body.appendChild(loadingBack);
-
-	// Loading screen
-	var loadingScreen = document.createElement("div");
-	loadingScreen.setAttribute("id", "loadingScreen");
-	document.body.appendChild(loadingScreen);
-
-	// Loading title
-	var loadingTitle = document.createElement("h2");
-	loadingTitle.setAttribute("id", "loadingTitle");
-	var loadingTitleText = document.createTextNode("LOADING...");
-	loadingTitle.appendChild(loadingTitleText);
-	loadingScreen.appendChild(loadingTitle);
-
-	// Loading para 1
-	var loadingPara1 = document.createElement("p");
-	loadingPara1.setAttribute("class", "loadingPara");
-	var loadingParaText1 = document.createTextNode("Veuillez opérer quelques instants, le Site va vous répondre...");
-	loadingPara1.appendChild(loadingParaText1);
-	loadingScreen.appendChild(loadingPara1);
-
-	// Loading para 2
-	var loadingPara2 = document.createElement("p");
-	loadingPara2.setAttribute("class", "loadingPara");
-	var loadingParaText2 = document.createTextNode("Faites défiler les éléments avec la molette ou utilisez les flèches (↑ ↓) de votre clavier. Cliquez pour sélectionner et déselectionner un élément ou utilisez les flèches (← →) de votre clavier.");
-	loadingPara2.appendChild(loadingParaText2);
-	loadingScreen.appendChild(loadingPara2);
-
-	// Loading close
-	var loadingClose = document.createElement("p");
-	loadingClose.setAttribute("id", "loadingClose");
-	loadingClose.setAttribute("onclick", "LoadingClose()");
-	var loadingCloseText = document.createTextNode("₪");
-	loadingClose.appendChild(loadingCloseText);
-	loadingScreen.appendChild(loadingClose);
-}
-
 // --- TILES BUILDING ---
 
 // [Title, Tile background, URL, Body background, Description title]
@@ -53,13 +9,15 @@ if (sessionStorage.getItem('loadingClosed') != 1){
 var tileCollection = [
 	["L A G", "./images/profil.jpg", "./html/contact.html", "", "Qui suis-je?"],
 	["VJing", "./images/articles/vjing.png", "./html/vjing.html", "./images/index/wallpaper_vjing.png", "Projet VJing"],
-	["A.R.M.", "./images/articles/arm.png", "./html/arm.html", "./images/index/wallpaper_arm.png", "Projet de jeu d'horreur"]
+	["A.R.M.", "./images/articles/arm.png", "./html/arm.html", "./images/index/wallpaper_arm.png", "Projet de jeu d'horreur"],
+	["Entomocene", "./images/entomocene/egout.jpg", "", "./images/index/wallpaper_virgo.png", "Projet de Rogue Like"]
 ];
 
 var tileDescription = [
 	["Bienvenue! Je m'appelle François GETE et je suis étudiant dans le jeu vidéo à <a href='https://ludus-academie.fr/' target='blank'>Ludus Académie</a>. Vous trouverez sur ce site mes différentes créations. Je vous invite donc à l'<span class='game' onclick='BeginGame()'>explorer</span>."],
 	["Le 'projet VJing' est un projet audio-visuel ayant pour but de lier le son et l'image dans une performance live. La particularité de celui-ci, est l'utilisation du son au service de la 3D."],
-	["Dans ce jeu, vous entrez dans la peau d'un employé de la ARM. Cette entreprise à pour but de répertorier et de classifier les objets considérés comme anormaux."]
+	["Dans ce jeu, vous entrez dans la peau d'un employé de la ARM. Cette entreprise à pour but de répertorier et de classifier les objets considérés comme anormaux."],
+	["Le <span class='game'>Site</span> n'est pas terminé pour le moment, veuillez nous en excuser."]
 ];
 
 // Scroller
--- js/index - loading.js
+++ js/index - loading.js
@@ -1,14 +1,60 @@
-function LoadingScreen(){
-	loadingTitle = document.getElementById("loadingTitle");
-	loadingClose = document.getElementById("loadingClose");
-	loadingPara1 = document.getElementsByClassName("loadingPara")[0];
+// --- LOADING BUILDING ---
 
-	loadingTitle.innerHTML = "LOADING... OK";
-	loadingPara1.innerHTML = "Le <span class='game'>Site</span> est prêt à vous accueillir."
-	loadingTitle.style.color = "#00FF00";
-	loadingClose.style.opacity = "100%";
-	loadingClose.style.pointerEvents = "auto";
-	loadingClose.innerHTML = "►";
+// If the loading screen has never been closed, build it
+if (sessionStorage.getItem('loadingClosed') != 1){
+	// Loading background
+	var loadingBack = document.createElement("div");
+	loadingBack.setAttribute("id", "loadingBack");
+	document.body.appendChild(loadingBack);
+
+	// Loading screen
+	var loadingScreen = document.createElement("div");
+	loadingScreen.setAttribute("id", "loadingScreen");
+	document.body.appendChild(loadingScreen);
+
+	// Loading title
+	var loadingTitle = document.createElement("h2");
+	loadingTitle.setAttribute("id", "loadingTitle");
+	var loadingTitleText = document.createTextNode("LOADING...");
+	loadingTitle.appendChild(loadingTitleText);
+	loadingScreen.appendChild(loadingTitle);
+
+	// Loading para 1
+	var loadingPara1 = document.createElement("p");
+	loadingPara1.setAttribute("class", "loadingPara");
+	var loadingParaText1 = document.createTextNode("Veuillez opérer quelques instants, le Site va vous répondre...");
+	loadingPara1.appendChild(loadingParaText1);
+	loadingScreen.appendChild(loadingPara1);
+
+	// Loading para 2
+	var loadingPara2 = document.createElement("p");
+	loadingPara2.setAttribute("class", "loadingPara");
+	var loadingParaText2 = document.createTextNode("Faites défiler les éléments avec la molette ou utilisez les flèches (↑ ↓) de votre clavier. Cliquez pour sélectionner et déselectionner un élément ou utilisez les flèches (← →) de votre clavier.");
+	loadingPara2.appendChild(loadingParaText2);
+	loadingScreen.appendChild(loadingPara2);
+
+	// Loading close
+	var loadingClose = document.createElement("p");
+	loadingClose.setAttribute("id", "loadingClose");
+	loadingClose.setAttribute("onclick", "LoadingClose()");
+	var loadingCloseText = document.createTextNode("₪");
+	loadingClose.appendChild(loadingCloseText);
+	loadingScreen.appendChild(loadingClose);
+}
+
+function LoadingScreen(){
+	if (sessionStorage.getItem('loadingClosed') != 1){
+		loadingTitle = document.getElementById("loadingTitle");
+		loadingClose = document.getElementById("loadingClose");
+		loadingPara1 = document.getElementsByClassName("loadingPara")[0];
+
+		loadingTitle.innerHTML = "LOADING... OK";
+		loadingPara1.innerHTML = "Le <span class='game'>Site</span> est prêt à vous accueillir."
+		loadingTitle.style.color = "#00FF00";
+		loadingClose.style.opacity = "100%";
+		loadingClose.style.pointerEvents = "auto";
+		loadingClose.innerHTML = "►";
+	}
 }
 
 function LoadingClose(){
