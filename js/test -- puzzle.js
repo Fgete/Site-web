1f44b5b16458e90160184d2fb1853a80350d4230
Lag <f.gete@ludus-academie.com>
2021-04-08 18:15:32 +0200

    site 3.11.4 3d interactive experiments

--- js/test -- puzzle.js
+++ js/test -- puzzle.js
@@ -1,15 +1,18 @@
 var origin = document.getElementById("origin");
 
-var ball = document.getElementById("ball");
-var cubeCenter = document.getElementById("cube1");
+var cube0 = document.getElementById("cube0");
 var intervalId = window.setInterval(Turn, 10);
 
 var rotationValue = 0;
 
 function Turn(){
-	origin.style.transform = "rotateY(" + (rotationValue*.1) + "deg)";
+	// origin.style.transform = "rotateY(" + (rotationValue*.1) + "deg) rotateX(-60deg)";
+	cube0.style.setProperty('--posY', "calc(var(--groundWidth)/-6 - " + (1+Math.sin(rotationValue/25))*20 + "px)");
 
-	ball.style.transform = "rotateY(" + (rotationValue*-.1) + "deg)";
-	cubeCenter.style.transform = "rotateY(" + rotationValue + "deg) rotateX(" + (rotationValue*1.1) + "deg)";
 	rotationValue++;
+}
+
+function SetCubeAt (x, z){
+	cube0.style.setProperty('--posX', x);
+	cube0.style.setProperty('--posZ', z);
 }
