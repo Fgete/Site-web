a00caeb5afddfc7186430d9caa075c6ad5b5a948
Lag <f.gete@ludus-academie.com>
2021-04-08 20:23:56 +0200

    site 3.11.5 3d again

--- js/test -- puzzle.js
+++ js/test -- puzzle.js
@@ -1,18 +1,41 @@
-var origin = document.getElementById("origin");
+// --- CONST ---
+const VELOCITY = .1;
 
-var cube0 = document.getElementById("cube0");
-var intervalId = window.setInterval(Turn, 10);
-
+// --- VARIABLES ---
 var rotationValue = 0;
+var posX = 0;
+var posZ = 0;
+var targetX = 0;
+var targetZ = 0;
 
-function Turn(){
+// --- ELEMENTS ---
+// Origin
+var origin = document.getElementById("origin");
+// Cube
+var cube0 = document.getElementById("cube0");
+
+// --- UPDATE ---
+var intervalId = window.setInterval(Update, 10);
+
+
+function Update(){
 	// origin.style.transform = "rotateY(" + (rotationValue*.1) + "deg) rotateX(-60deg)";
 	cube0.style.setProperty('--posY', "calc(var(--groundWidth)/-6 - " + (1+Math.sin(rotationValue/25))*20 + "px)");
 
-	rotationValue++;
+	SetCubeAt(targetX - (targetX-posX)/(1+VELOCITY) , targetZ - (targetZ-posZ)/(1+VELOCITY));
+
+	rotationValue++; // ITERATION
 }
 
-function SetCubeAt (x, z){
-	cube0.style.setProperty('--posX', x);
-	cube0.style.setProperty('--posZ', z);
+// Set element (cube) to x & z coordonates
+function SetCubeAt (newX, newZ){
+	posX = newX;
+	posZ = newZ;
+	cube0.style.setProperty('--posX', "calc(var(--groundWidth)/3 * " + posX + ")");
+	cube0.style.setProperty('--posZ', "calc(var(--groundWidth)/3 * " + posZ + ")");
+}
+
+function GoToXZ (newX, newZ){
+	targetX = newX;
+	targetZ = newZ;
 }
