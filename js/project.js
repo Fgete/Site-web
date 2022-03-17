7bfb9f48993a30bb7a982fba7c20e09b9665b4f4
Lag <f.gete@ludus-academie.com>
2022-03-17 14:29:36 +0100

    site 4.0.6

--- js/project.js
+++ js/project.js
@@ -12,6 +12,9 @@ function EnterProject(){
     mainScreen.style.width      = "88%";
     mainScreen.style.transform  = "translateZ(-1000px) translateX(105px) rotateY(10deg)";
     mainScreen.style.pointerEvents = "";
+    setTimeout(function () {
+        window.scrollTo(0, 120);
+    }, 1000);
 }
 
 function ExitProject(){
@@ -20,4 +23,9 @@ function ExitProject(){
     mainScreen.style.width      = "100%";
     mainScreen.style.transform  = "none";
     mainScreen.style.pointerEvents = "none";
+}
+
+function GoUp() {
+    console.log("ScrollUp");
+    window.scrollTo(0, 0);
 }
